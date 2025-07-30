4cd3f637cb9ab271eb2f369ea359cd9a910c862b
NexxusFz <farhanzulkarnaenhrp@gmail.com>
2025-07-30 09:41:50 +0700

    fix same

--- src/app/dashboard/admin-store/store/[storeId]/page.tsx
+++ src/app/dashboard/admin-store/store/[storeId]/page.tsx
@@ -1,17 +1,11 @@
-// File: app/(store-admin)/store/[storeId]/page.tsx
-
-"use client";
-
 import MenuNavbarStoreAdmin from "@/components/header/header-admin-store/header-admin-store";
 import StoreDetailPage from "@/components/menu/menu-store-admin/store-by-id";
 
-interface PageProps {
-  params: {
-    storeId: string;
-  };
-}
-
-export default function StoreByIdPage({ params }: PageProps) {
+export default function StoreByIdPage({
+  params,
+}: {
+  params: { storeId: string };
+}) {
   return (
     <MenuNavbarStoreAdmin>
       <StoreDetailPage params={params} />
