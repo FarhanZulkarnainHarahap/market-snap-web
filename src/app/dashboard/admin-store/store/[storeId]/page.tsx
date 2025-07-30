24e2d5110f92d4c39a75ef448c1d5d48f5a68b4e
NexxusFz <farhanzulkarnaenhrp@gmail.com>
2025-07-30 10:01:47 +0700

    fix vercel

--- src/app/dashboard/admin-store/store/[storeId]/page.tsx
+++ src/app/dashboard/admin-store/store/[storeId]/page.tsx
@@ -1,11 +1,12 @@
 import MenuNavbarStoreAdmin from "@/components/header/header-admin-store/header-admin-store";
 import StoreDetailPage from "@/components/menu/menu-store-admin/store-by-id";
 
-interface PageProps {
+// ✅ Langsung definisikan props secara eksplisit
+export default function StoreByIdPage({
+  params,
+}: {
   params: { storeId: string };
-}
-
-export default function StoreByIdPage({ params }: PageProps) {
+}) {
   return (
     <MenuNavbarStoreAdmin>
       <StoreDetailPage storeId={params.storeId} />
