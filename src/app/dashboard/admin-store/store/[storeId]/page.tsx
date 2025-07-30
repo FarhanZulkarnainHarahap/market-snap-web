5f83503e9ac7ecffc1c771c9db399137bcfd6b86
NexxusFz <farhanzulkarnaenhrp@gmail.com>
2025-07-30 09:47:32 +0700

    fix storeId

--- src/app/dashboard/admin-store/store/[storeId]/page.tsx
+++ src/app/dashboard/admin-store/store/[storeId]/page.tsx
@@ -1,11 +1,13 @@
 import MenuNavbarStoreAdmin from "@/components/header/header-admin-store/header-admin-store";
 import StoreDetailPage from "@/components/menu/menu-store-admin/store-by-id";
 
-export default function StoreByIdPage({
-  params,
-}: {
-  params: { storeId: string };
-}) {
+type StoreIdParams = {
+  params: {
+    storeId: string;
+  };
+};
+
+export default function StoreByIdPage({ params }: StoreIdParams) {
   return (
     <MenuNavbarStoreAdmin>
       <StoreDetailPage params={params} />
